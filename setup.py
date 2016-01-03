79e98b5522e07c23c9af44987375db4b25086d82
Nathan Yergler <nathan@yergler.net>
2016-01-03 14:17:48 -0800

    Initial implementation.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lektor-hidden-attachments',
+    version='0.1',
+    author=u'Nathan Yergler',
+    author_email='nathan@yergler.net',
+    license='MIT',
+    py_modules=['lektor_hidden_attachments'],
+    entry_points={
+        'lektor.plugins': [
+            'hidden-attachments = lektor_hidden_attachments:HiddenAttachmentsPlugin',
+        ]
+    }
+)
